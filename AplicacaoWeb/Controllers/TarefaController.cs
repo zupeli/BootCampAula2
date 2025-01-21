2ec9831760199f49b6f698113d66c54e3fe1b65e
ElvysZupeli <zupeli@yahoo.com.br>
2025-01-21 20:09:21 -0300

    primeito commit

---
+++ AplicacaoWeb/Controllers/TarefaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TarefaController : ControllerBase
+    {
+        // GET: /Tarefa
+        [HttpGet]
+        public IActionResult GetTarefas()
+        {
+            var tarefas = new[]
+            {
+                 new { Id = 1, Nome = "Tarefa 1", Concluida = false },
+                 new { Id = 2, Nome = "Tarefa 2", Concluida = true  },
+                 new { Id = 3, Nome = "Tarefa 3", Concluida = false }
+ };
+            return Ok(tarefas);
+        }
+    }
+}
